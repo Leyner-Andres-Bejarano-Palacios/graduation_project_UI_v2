d428968b0e58d3880381e68cbe5d8be0dafc143a
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-10-16 22:09:42 -0500

    doing some modifications to make it look like I need to looks like

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TaskService } from '../task.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,9 +11,77 @@ import { TaskService } from '../task.service';
 export class UserProfileComponent implements OnInit {
 
   task: Task = new Task();
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService,
+    private toastr: ToastrService) { }
+  showNotification(from, align){
 
-  ngOnInit() {
+    const color = Math.floor((Math.random() * 5) + 1);
+
+    switch(color){
+      case 1:
+      this.toastr.info('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+         timeOut: 8000,
+         closeButton: true,
+         enableHtml: true,
+         toastClass: "alert alert-info alert-with-icon",
+         positionClass: 'toast-' + from + '-' +  align
+       });
+      break;
+      case 2:
+      this.toastr.success('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+         timeOut: 8000,
+         closeButton: true,
+         enableHtml: true,
+         toastClass: "alert alert-success alert-with-icon",
+         positionClass: 'toast-' + from + '-' +  align
+       });
+      break;
+      case 3:
+      this.toastr.warning('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+         timeOut: 8000,
+         closeButton: true,
+         enableHtml: true,
+         toastClass: "alert alert-warning alert-with-icon",
+         positionClass: 'toast-' + from + '-' +  align
+       });
+      break;
+      case 4:
+      this.toastr.error('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+         timeOut: 8000,
+         enableHtml: true,
+         closeButton: true,
+         toastClass: "alert alert-danger alert-with-icon",
+         positionClass: 'toast-' + from + '-' +  align
+       });
+       break;
+       case 5:
+       this.toastr.show('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+          timeOut: 8000,
+          closeButton: true,
+          enableHtml: true,
+          toastClass: "alert alert-primary alert-with-icon",
+          positionClass: 'toast-' + from + '-' +  align
+        });
+      break;
+      default:
+      break;
+    }
+}
+
+  ngOnInit(): void {
+  }
+
+
+  saveTask(){
+    this.taskService.createTask(this.task).subscribe( data =>{
+      console.log(data);
+    },
+    error => console.log(error));
+  }
+
+  onSubmit(){
+    console.log(this.task);
+    this.saveTask();
   }
 
 }
